78e699bb7a5153449a35516cf0520cc7c3165fe3
Yifan Wu <tyivanwu@gmail.com>
2018-06-19 00:07:07 -0400

    combine inject_on_book.js and inject_on_reader.js

--- chrome/js/inject_on_book.js
+++ chrome/js/inject_on_book.js
@@ -3,7 +3,7 @@ const url = window.location.toString();
 const id = url.split("/")[5];
 const language = url.split("/")[3];
 const pdf_url =
-  "http://taiwanebook.ncl.edu.tw/ebkFiles/" + id + "/" + id + ".PDF";
+    "http://taiwanebook.ncl.edu.tw/ebkFiles/" + id + "/" + id + ".PDF";
 
 const get_label = (language) => {
     if (language === "en") {
@@ -13,18 +13,28 @@ const get_label = (language) => {
     }
 };
 
-const label = get_label(language)
+const label = get_label(language);
 
-// add download button
 let download_icon = document.createElement("I");
-download_icon.classList.add('right', 'chevron', 'icon');
 
 let download_btn = document.createElement("A");
-download_btn.classList.add('ui', 'cetered', 'blue', 'button', 'focus_border');
 download_btn.target = "_blank";
 download_btn.href = pdf_url;
 download_btn.textContent = label;
 download_btn.appendChild(download_icon);
 
-let book_cover = document.querySelector("div.ui.medium.image");
-book_cover.appendChild(download_btn);
+if (url.split("/").length === 6) {
+    // on book page
+    download_icon.classList.add("right", "chevron", "icon");
+    download_btn.classList.add("ui", "cetered", "blue", "button", "focus_border");
+    let book_cover = document.querySelector("div.ui.medium.image");
+    book_cover.appendChild(download_btn);
+} else if (url.split("/").length === 7) {
+    // on book reader page
+    download_icon.classList.add("right", "arrow", "icon");
+    download_btn.classList.add("ui", "labeled", "icon", "button", "focus_border");
+    let btn_column = document.querySelector(
+        "body > main > main > div > div:nth-child(1) > div.ui.three.wide.computer.three.wide.tablet.sixteen.wide.mobile.column"
+    );
+    btn_column.appendChild(download_btn);
+}
