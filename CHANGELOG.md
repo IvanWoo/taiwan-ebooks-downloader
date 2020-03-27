3f85a4ddd0aba985c51d17f5aac0b46830937384
Yifan Wu <tyivanwu@gmail.com>
2020-03-27 00:55:22 -0400

    fix: add Referer into requestHeaders

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+# 0.3.0
+
+### Bug Fixes
+- add **Referer** into requestHeaders
--- chrome/background.js
+++ chrome/background.js
@@ -1,4 +1,23 @@
-chrome.browserAction.onClicked.addListener(activeTab => {
-    let newURL = 'http://taiwanebook.ncl.edu.tw';
+chrome.browserAction.onClicked.addListener((activeTab) => {
+    let newURL = "http://taiwanebook.ncl.edu.tw";
     chrome.tabs.create({ url: newURL });
 });
+
+// https://developer.chrome.com/extensions/webRequest
+// https://stackoverflow.com/a/59296870
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    (details) => {
+        const newHeader = {
+            name: "Referer",
+            value: "https://taiwanebook.ncl.edu.tw"
+        };
+        const requestHeaders = details.requestHeaders.concat(newHeader);
+        return { requestHeaders };
+    },
+    // filters
+    {
+        urls: ["https://taiwanebook.ncl.edu.tw/ebkFiles/*"]
+    },
+    // extraInfoSpec
+    ["blocking", "requestHeaders", "extraHeaders"]
+);
